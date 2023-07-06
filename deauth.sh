c1d32009e36a8fa67130dc9c76b74a8fe189d4d1
TakkosTakko <90936936+TakkosTakko@users.noreply.github.com>
2023-07-06 17:28:37 +0200

    Add files via upload
    added some things for better usage

--- deauth.sh
+++ deauth.sh
@@ -2,6 +2,11 @@
 
 # Begrüßung
 echo "Willkommen zum Autoinstaller zum DeAuthen von WPA2/WPA3 Netzwerken!"
+echo "JUST for Educational things donst use it for cracking its illegal"
+echo "JUST for Educational things donst use it for cracking its illegal"
+echo "JUST for Educational things donst use it for cracking its illegal"
+echo "JUST for Educational things donst use it for cracking its illegal"
+echo "JUST for Educational things donst use it for cracking its illegal"
 echo " "
 
 echo "  _____                     _   _                                                                                "
@@ -12,11 +17,11 @@ echo " | |__| |  __// ____ \ |_| | |_| | | |                                    
 echo " |_____/ \___/_/  _ \_\__,_|\__|_| |_|   _               _             ______     _ _  __           _    _       "
 echo " |_   _|         | |      | | |     | | (_)             | |           |  ____|   (_) |/ /          | |  (_)      "
 echo "   | |  _ __  ___| |_ __ _| | | __ _| |_ _  ___  _ __   | |__  _   _  | |____   ___| ' / ___   ___ | | ___  ___  "
-echo "   | | | '_ \/ __| __/ _` | | |/ _` | __| |/ _ \| '_ \  | '_ \| | | | |  __\ \ / / |  < / _ \ / _ \| |/ / |/ _ \ "
+echo "   | | | '_ \/ __| __/ _` I I I/ _` | __| |/ _ \| '_ \  | '_ \| | | | |  __\ \ / / |  < / _ \ / _ \| |/ / |/ _ \ "
 echo "  _| |_| | | \__ \ || (_| | | | (_| | |_| | (_) | | | | | |_) | |_| | | |___\ V /| | . \ (_) | (_) |   <| |  __/ "
 echo " |_____|_| |_|___/\__\__,_|_|_|\__,_|\__|_|\___/|_| |_| |_.__/ \__, | |______\_/ |_|_|\_\___/ \___/|_|\_\_|\___|" 
-echo"                                                           __/ |                                            "
-echo"                                                          |___/                                             "
+echo "                                                          __/ |                                            "
+echo "                                                         |___/                                             "
 
 # Abfrage, ob der Code ausgeführt werden soll
 read -p "Möchtest du die Installation ausführen? (ja/nein): " execute
@@ -24,31 +29,43 @@ read -p "Möchtest du die Installation ausführen? (ja/nein): " execute
 # Überprüfen der Benutzereingabe
 if [[ "$execute" == "ja" ]]; then
     echo "Installation wird ausgeführt..."
+
     echo "Anmeldung als root" 
 	sudo su -i root   
 	
 	#Installieren des Dependencies
     echo "Installation von den Dependencies"
 	sudo apt install autoconf automake libpcre3-dev libnl-3-dev libsqlite3-dev libssl-dev ethtool build-essential g++ libnl-genl-3-dev libgcrypt20-dev libtool python3-distutils
-        sudo apt install -y pkg-config
+    sudo apt install -y pkg-config
+
     # Installation der benötigten Pakete
     echo "Updaten des Servers"
-    sudo apt-get update
+    sudo apt-get update && apt-get upgrade -y
     
     echo "Installieren von aircrack-ng"
     sudo apt-get install -y aircrack-ng
-apt install git
-git clone https://github.com/aircrack-ng/aircrack-ng.git
-cd aircrack-ng
-sudo autoreconf -i
-sudo ./configure --with-experimental --with-ext-scripts 
-sudo make 
-sudo make install
-sudo airodump-ng-oui-update
-        echo "Installieren von mdk4 && mdk3"
+    apt install git
+    git clone https://github.com/aircrack-ng/aircrack-ng.git
+    cd aircrack-ng
+    sudo autoreconf -i
+    sudo ./configure --with-experimental --with-ext-scripts 
+    sudo make 
+    sudo make install
+    sudo airodump-ng-oui-update
+    # Installation der mdk4 && mdk3 packets
+    echo "Installieren von mdk4 && mdk3"
     sudo apt-get install -y mdk4 mdk3
+
+    echo "Installieren von Netzwerktreiber (RTL8812AU)"
+    sudo apt-get install dkms
+    git clone -b v5.6.4.2 https://github.com/aircrack-ng/rtl8812au.git
+    cd rtl*
+    sudo make dkms_install
+    make && make install
+    sudo apt-get update
+    sudo apt-get install bc mokutil build-essential libelf-dev linux-headers-`uname -r`
 
     echo "Installation abgeschlossen. Server muss neugestartet werden!"
 else
-    echo "Code wird nicht ausgeführt. Skript wird beendet."
+    echo "Fehler beim Ausführen des Skripts, Skript wird beendet."
 fi
